1757afcfb6b1f5ac1149fa5a82a92b6517b118a5
DuloeW <91319086+DuloeW@users.noreply.github.com>
2022-03-01 20:00:12 +0800

    Penambahan di html dan perbaikan di js

--- percobaankeEmpat.js
+++ percobaankeEmpat.js
@@ -1,43 +1,65 @@
+var identitas = prompt("Masukan Nama Kamu")
 var keputusan = prompt("Masukan Pilihan Kamu! \n Perkalian = 1 \n Pembagian = 2 \n Penjumlahan = 3 \n Pengurangan = 4 \n Rumus = 5");
 
 switch (keputusan) {
   case '1':
-    alert("Oke kamu memilih Perkalian");
-    var kali = prompt("Masukan perkalian berapa yang anda mau \n Hanya sampai perkalian 10");
-      switch (kali) {
-        case '1':
-          alert("Perkalian 1 \n  1 x 1 = 1 \n  1 x 2 = 2 \n  1 x 3 = 3 \n  1 x 4 = 4 \n  1 x 5 = 5 \n  1 x 6 = 6 \n  1 x 7 = 7 \n  1 x 8 = 8 \n  1 x 9 = 9 \n  1 x 10 = 10 \n")
+    var kepKali = prompt("Kamu memilih perkalian : \n =>Perkalian Manual ( 1 ) \n =>Perkalian Instan ( 2 )")
+        switch (kepKali) {
+          case '1':
+            var nilaiKali = parseInt(prompt("Perkalian berapa"))
+            var ulngKali  = parseInt(prompt("sampai berapa"))
+                for (let i = 1; i <= ulngKali; i++) {
+                var hasil = i * nilaiKali
+                  if (i == 10) {
+                   alert("Selamat Menikmati Mengetuk Layar") 
+                  }else if(i == ulngKali){
+                    alert("2 Kali ketukan lagi untuk menyelesaikan ini")
+                  }
+                alert(`${nilaiKali} x ${i} = ${hasil} `);
+              }
           break;
         case '2':
-          alert("Perkalian 2 \n  2 x 1 = 2 \n  2 x 2 = 4 \n  2 x 3 = 6 \n  2 x 4 = 8 \n  2 x 5 = 10 \n  2 x 6 = 12 \n  2 x 7 = 14 \n  2 x 8 = 16 \n  2 x 9 = 18 \n  2 x 10 = 20 \n")
-          break;
-        case '3':
-          alert("Perkalian 3 \n  3 x 1 = 3 \n  3 x 2 = 6 \n  3 x 3 = 9 \n  3 x 4 = 12 \n  3 x 5 = 15 \n  3 x 6 = 18 \n  3 x 7 = 21 \n  3 x 8 = 24 \n  3 x 9 = 27 \n  3 x 10 = 30 \n")
-          break;
-        case '4':
-          alert("Perkalian 4 \n  4 x 1 = 4 \n  4 x 2 = 8 \n  4 x 3 = 12 \n  4 x 4 = 16 \n  4 x 5 = 20 \n  4 x 6 = 24 \n  4 x 7 = 28 \n  4 x 8 = 32 \n  4 x 9 = 36 \n  4 x 10 = 40 \n")
-          break;
-        case '5':
-          alert("Perkalian 5 \n  5 x 1 = 5 \n  5 x 2 = 10 \n  5 x 3 = 15 \n  5 x 4 = 20 \n  5 x 5 = 25 \n  5 x 6 = 30 \n  5 x 7 = 35 \n  5 x 8 = 40 \n  5 x 9 = 45 \n  5 x 10 = 50 \n")
-          break;
-        case '6':
-          alert("Perkalian 6 \n  6 x 1 = 6 \n  6 x 2 = 12 \n  6 x 3 = 18 \n  6 x 4 = 24 \n  6 x 5 = 30 \n  6 x 6 = 36 \n  6 x 7 = 42 \n  6 x 8 = 48 \n  6 x 9 = 54 \n  6 x 10 = 60 \n")
-          break;
-        case '7':
-          alert("Perkalian 7 \n  7 x 1 = 7 \n  7 x 2 = 14 \n  7 x 3 = 21 \n  7 x 4 = 28 \n  7 x 5 = 35 \n  7 x 6 = 42 \n  7 x 7 = 49 \n  7 x 8 = 56 \n  7 x 9 = 63 \n  7 x 10 = 70 \n")
-          break;
-        case '8':
-          alert("Perkalian 8 \n  8 x 1 = 8 \n  8 x 2 = 16 \n  8 x 3 = 24 \n  8 x 4 = 32 \n 8 x 5 = 40 \n  8 x 6 = 48 \n  8 x 7 = 56 \n  8 x 8 = 64 \n  8 x 9 = 72 \n  8 x 10 = 80 \n")
-          break;
-        case '9':
-          alert("Perkalian 9 \n  9 x 1 = 9 \n  9 x 2 = 18 \n  9 x 3 = 27 \n  9 x 4 = 36 \n  9 x 5 = 45 \n  9 x 6 = 54 \n  9 x 7 = 63 \n  9 x 8 = 72 \n  9 x 9 = 81 \n 9 x 10 = 90 \n")
-          break;
-        case '10':
-          alert("Perkalian 10 \n  10 x 1 = 10 \n  10 x 2 = 20 \n  10 x 3 = 30 \n  10 x 4 = 40 \n  10 x 5 = 50 \n  10 x 6 = 60 \n  10 x 7 = 70 \n  10 x 8 = 80 \n  10 x 9 = 90 \n  10 x 10 = 100 \n")
+          alert("Oke kamu memilih Perkalian");
+          var kali = prompt("Masukan perkalian berapa yang anda mau \n Hanya sampai perkalian 10");
+          switch (kali) {
+            case '1':
+              alert("Perkalian 1 \n  1 x 1 = 1 \n  1 x 2 = 2 \n  1 x 3 = 3 \n  1 x 4 = 4 \n  1 x 5 = 5 \n  1 x 6 = 6 \n  1 x 7 = 7 \n  1 x 8 = 8 \n  1 x 9 = 9 \n  1 x 10 = 10 \n")
+              break;
+            case '2':
+              alert("Perkalian 2 \n  2 x 1 = 2 \n  2 x 2 = 4 \n  2 x 3 = 6 \n  2 x 4 = 8 \n  2 x 5 = 10 \n  2 x 6 = 12 \n  2 x 7 = 14 \n  2 x 8 = 16 \n  2 x 9 = 18 \n  2 x 10 = 20 \n")
+              break;
+            case '3':
+              alert("Perkalian 3 \n  3 x 1 = 3 \n  3 x 2 = 6 \n  3 x 3 = 9 \n  3 x 4 = 12 \n  3 x 5 = 15 \n  3 x 6 = 18 \n  3 x 7 = 21 \n  3 x 8 = 24 \n  3 x 9 = 27 \n  3 x 10 = 30 \n")
+              break;
+            case '4':
+              alert("Perkalian 4 \n  4 x 1 = 4 \n  4 x 2 = 8 \n  4 x 3 = 12 \n  4 x 4 = 16 \n  4 x 5 = 20 \n  4 x 6 = 24 \n  4 x 7 = 28 \n  4 x 8 = 32 \n  4 x 9 = 36 \n  4 x 10 = 40 \n")
+              break;
+            case '5':
+              alert("Perkalian 5 \n  5 x 1 = 5 \n  5 x 2 = 10 \n  5 x 3 = 15 \n  5 x 4 = 20 \n  5 x 5 = 25 \n  5 x 6 = 30 \n  5 x 7 = 35 \n  5 x 8 = 40 \n  5 x 9 = 45 \n  5 x 10 = 50 \n")
+              break;
+            case '6':
+              alert("Perkalian 6 \n  6 x 1 = 6 \n  6 x 2 = 12 \n  6 x 3 = 18 \n  6 x 4 = 24 \n  6 x 5 = 30 \n  6 x 6 = 36 \n  6 x 7 = 42 \n  6 x 8 = 48 \n  6 x 9 = 54 \n  6 x 10 = 60 \n")
+              break;
+            case '7':
+              alert("Perkalian 7 \n  7 x 1 = 7 \n  7 x 2 = 14 \n  7 x 3 = 21 \n  7 x 4 = 28 \n  7 x 5 = 35 \n  7 x 6 = 42 \n  7 x 7 = 49 \n  7 x 8 = 56 \n  7 x 9 = 63 \n  7 x 10 = 70 \n")
+              break;
+            case '8':
+              alert("Perkalian 8 \n  8 x 1 = 8 \n  8 x 2 = 16 \n  8 x 3 = 24 \n  8 x 4 = 32 \n 8 x 5 = 40 \n  8 x 6 = 48 \n  8 x 7 = 56 \n  8 x 8 = 64 \n  8 x 9 = 72 \n  8 x 10 = 80 \n")
+              break;
+            case '9':
+              alert("Perkalian 9 \n  9 x 1 = 9 \n  9 x 2 = 18 \n  9 x 3 = 27 \n  9 x 4 = 36 \n  9 x 5 = 45 \n  9 x 6 = 54 \n  9 x 7 = 63 \n  9 x 8 = 72 \n  9 x 9 = 81 \n 9 x 10 = 90 \n")
+              break;
+            case '10':
+              alert("Perkalian 10 \n  10 x 1 = 10 \n  10 x 2 = 20 \n  10 x 3 = 30 \n  10 x 4 = 40 \n  10 x 5 = 50 \n  10 x 6 = 60 \n  10 x 7 = 70 \n  10 x 8 = 80 \n  10 x 9 = 90 \n  10 x 10 = 100 \n")
+              break;
+            default:
+              alert("HANYA SAMPAI PERKALIAN 10")
+          }
           break;
         default:
-          alert("HANYA SAMPAI PERKALIAN 10")
+          alert("Pilih no 1 dan 2 aja ya kak")
       }
+      
     break;
   case '2':
     alert("Oke kamu memilih Pembagian \n (nilai pertama : nila kedua)");
@@ -48,6 +70,7 @@ switch (keputusan) {
     var persen = confirm("Mau tau hasil dari bagi kamu jika di ubah ke bentuk pesen(%)?")
         if (persen == true) {
           var persenBag = hasilBagi * 1 / 100
+
           alert("Hasil bagi kamu jika di jadikan persen maka akan menjadi " + persenBag + "%")
         }else if(persen == false){
           alert("Oke, jika mau mengulang tinggal refresh halaman ini")
@@ -82,7 +105,7 @@ switch (keputusan) {
     alert("Nilai kurang pertama kamu " + kurang1 + " dan nilai kurang kedua kamu " + kurang2 + " maka hasil jumlah kamu adalah " + hasilKurang + "\n (" + kurang1 + " - " + kurang2 + " = " + hasilKurang +")");
     var latKurang = confirm("Mau latihan pengurangan?")
         if (latKurang == true) {
-          var level = prompt("Silahkan pilih level pengurangannya \n level : \n 1 \n 2 \n 3 \n 4")
+          var level = prompt("Silahkan pilih level pengurangannya \n level : \n 1 \n 2 \n 3 \n 4 \n 5")
             switch (level) {
               case '1':
                 var solKurang1 = Math.round(Math.random());
@@ -128,24 +151,52 @@ switch (keputusan) {
                     alert("BLOKKKK BLOKKKK BLOKKKK")
                   }
                 break;
+              case '5':
+                if (level == 5) {
+                  var lev5Kep = confirm("Yakin level 5?")
+                  if (lev5Kep == true) {
+                    alert("Selamat datang di level dewa matematika")
+                    alert("Dan ini adalah pengurangan yang akan di campur dengan perkalian dan pembagian")
+                    alert("Mari kita mulai")
+                    alert("3")
+                    alert("2")
+                    alert("1")
+                    var solKe1 = Math.round(Math.random() * 1000)
+                    var solKe2 = Math.round(Math.random() * 100 - 8)
+                    var solKe3 = Math.round(Math.random() * 1000 )
+                    var ranHasil = solKe1 * solKe3 - solKe1 / solKe2
+                    var soalRan = prompt(solKe1 + " x " + solKe3 + " - " + solKe1 + " : " + solKe2 + " = ?" )
+                      if (soalRan == ranHasil) {
+                        alert("Ampun suhu kamu sudah menamatkan level dewa")
+                      }else{
+                        alert("Trimakasih Telah Mencoba Level Dewa")
+                      }
+                  }else{
+                    alert("Takut Ya?")
+                  }
+                }else{
+                  alert("Jangan Typo")
+                }
+                break;
+              
               default:
-                alert("CUMAN SAMPAII LEVEL 4 AJA YA KAK \n jangan Typo")
+                alert("CUMAN SAMPAII LEVEL 5 AJA YA KAK \n jangan Typo")
             }
         }
     break;
   case '5':
-    var penentu5Case = prompt("Silahkan mau memilih rumus apa? \n => Luas \n => Keliling")
+    var penentu5Case = prompt("Silahkan mau memilih rumus apa? \n => Luas ( 1 ) \n => Keliling ( 2 )")
       switch (penentu5Case) {
-        case 'Luas':
-          var pilLuas = prompt("Luas : \n => Persegi \n => Persegi Panjang \n => Segitiga")
+        case '1':
+          var pilLuas = prompt("Luas : \n => Persegi ( 1 ) \n => Persegi Panjang ( 2 ) \n => Segitiga ( 3 )")
             switch (pilLuas) {
-              case 'Persegi':
+              case '1':
                 alert("Oke, kamu memilih rumus persegi");
                 var sisi1 = parseInt(prompt("Masukan sisi dari persegi ini 🟥"))
                 var hasilPer = sisi1 * sisi1
                 alert("Kamu memasukan sisi persegi ini 🟥 dengan panjang " + sisi1 + "cm, \nJadi luas dari persegi yang memiliki sisi " + sisi1 + "cm adalah " + hasilPer + "cm")
                 break;
-             case 'Persegi Panjang':
+             case '2':
                alert("Oke, kamu memilih rumus persegi panjang")
                function lebarPerJang(p,l){
                  return p * l;
@@ -155,7 +206,7 @@ switch (keputusan) {
                var hasil = lebarPerJang(p,l)
                alert("=>Kamu memasukan panjang dari persegi panjang ini 🎴 dengan panjang " + p + "cm, \n=>Dan kamu memasukan lebar dari persegi panjang ini 🎴 dengan lebar " + l + "cm \n=>Jadi, luas dari persegi panjang yang memiliki panjang " + p + "cm dan memiliki lebar " + l + "cm adalah " + hasil + "cm")
                break;
-            case 'Segitiga':
+            case '3':
               alert("Oke, kamu memilih rumus segitiga")
               function luasSegitiga(a,t){
                 return 1 * ( a * t) / 2
@@ -169,16 +220,16 @@ switch (keputusan) {
               alert("Hanya ada Persegi, Persegi Panjang, Segitiga \n jangan Typo");
           }
           break;
-        case 'Keliling':
-          var pilKeling = prompt("Keliling : \n => Persegi \n => Persegi Panjang \n => Segitiga")
+        case '2':
+          var pilKeling = prompt("Keliling : \n => Persegi ( 1 ) \n => Persegi Panjang ( 2 ) \n => Segitiga ( 3 )")
             switch (pilKeling) {
-              case 'Persegi':
+              case '1':
                 alert("Oke, kamu memilih rumus persegi");
                 var sisi1 = parseInt(prompt("Masukan sisi dari persegi ini 🟥"))
                 var hasilPer = sisi1 + sisi1 + sisi1 + sisi1
                 alert("=>Kamu memasukan sisi persegi ini 🟥 dengan panjang " + sisi1 + "cm, \n=>Jadi, Keliling dari persegi yang memiliki sisi " + sisi1 + "cm adalah " + hasilPer + "cm")
                 break;
-              case 'Persegi Panjang':
+              case '2':
                 alert("Oke, kamu memilih rumus persegi panjang")
                 function kelilingPerJang(p,l){
                   return 2 * (p + l);
@@ -188,7 +239,7 @@ switch (keputusan) {
                 var hasil = kelilingPerJang(p,l)
                 alert("=>Kamu memasukan panjang dari persegi panjang ini 🎴 dengan panjang " + p + "cm, \n=>Dan kamu memasukan lebar dari persegi panjang ini 🎴 dengan lebar " + l + "cm \n=>Jadi, keliling dari persegi panjang yang memiliki panjang " + p + "cm dan memiliki lebar " + l + "cm adalah " + hasil + "cm")
                 break;
-              case 'Segitiga':
+              case '3':
                 alert("Oke, kamu memilih rumus segitiga")
                 function kelilingSegitiga(a,b,c){
                   return a + b + c
@@ -208,10 +259,23 @@ switch (keputusan) {
       }
     break;
   default:
-    alert("Jangan Typo")
+    alert("Hanya Sampai 5")
 }
-document.getElementById('text').innerHTML = "Terimakasih telah mencoba mesin kami";
+//manipulasi penilaian
+var penilaian = prompt("Tolong Nilai Games Ini \n⭐ \n⭐⭐ \n⭐⭐⭐ \n⭐⭐⭐⭐ \n⭐⭐⭐⭐⭐    ");
+  if(penilaian == '⭐'){
+    document.getElementById('bintang').innerHTML = "Terimakasi atas " + penilaian + ",  kami akan memperbaiki game ini"
+  }else if(penilaian == '⭐⭐'){
+    document.getElementById('bintang').innerHTML = "Terimakasi atas " + penilaian + ",  kami akan memperbaiki game ini"
+  }else if(penilaian == '⭐⭐⭐'){
+    document.getElementById('bintang').innerHTML = "Terimakasi atas " + penilaian + ",  kami akan memperbaiki game ini"
+  }else if(penilaian == '⭐⭐⭐⭐'){
+    document.getElementById('bintang').innerHTML = "Terimakasi atas " + penilaian + ",  kami akan memperbaiki game ini"
+  }else if(penilaian == '⭐⭐⭐⭐⭐'){
+    document.getElementById('bintang').innerHTML = "Terimakasi atas " + penilaian + ",  kami akan memperbaiki game ini"
+  }
+document.getElementById('text').innerHTML = "Terimakasih " + identitas + " telah mencoba mesin kami";
 document.getElementById('emot').innerHTML =
-"😇🙏"
+"😇 \n 🙏"
 document.getElementById('copy-right').innerHTML =
 "©2022-Bayu Code"
